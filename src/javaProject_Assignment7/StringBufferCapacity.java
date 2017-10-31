fd18687a1c7e28d3e2e1d06999deb1a0d54a59c9
gunarora <gunjanarrora@gmail.com>
2017-10-31 16:40:23 +0530

    Update StringBufferCapacity.java

--- src/javaProject_Assignment7/StringBufferCapacity.java
+++ src/javaProject_Assignment7/StringBufferCapacity.java
@@ -1,6 +1,10 @@
 //Package declaration is mandatory
 package javaProject_Assignment7;
 
+/*Write a program in java to show how the capacity is changing in StringBuffer or in StringBuilder if we
+append the string.
+Hint: - The capacity changes according to the formula = Initial_capacity*2+2.*/
+
 public class StringBufferCapacity {
 
 	//Main method started
